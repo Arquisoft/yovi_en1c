76724991cdc13be9d57533cafd9abd44e62c1713
Elif Busra Caylan <elifbusracaylan@gmail.com>
2026-04-17 00:10:46 +0200

    fix:align jwt secrets and fix 403 error for bot moves

--- users/users-service.js
+++ users/users-service.js
@@ -34,7 +34,7 @@ const Game = mongoose.models.Game || mongoose.model("Game", GameSchema);
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   if (req.method === "OPTIONS") return res.sendStatus(204);
   next();
 });
